51d857af552bf2bba02ba2c605fe2ce0a00f4ef1
Aaron Teng <aaronteng@berkeley.edu>
2026-05-27 18:54:02 -0700

    Logos lexer

--- src/shell.rs
+++ src/shell.rs
@@ -1,3 +1,7 @@
+mod lexer;
+
+use lexer::{Tkn, LexerState};
+use logos::{Logos, Lexer};
 use rustyline::{DefaultEditor};
 use rustyline::error::ReadlineError;
 use rustyline::history::{History, SearchDirection};
@@ -14,8 +18,10 @@ const EDITOR_HISTORY: &str = "./shell_cmd_history.txt";
 
 struct ChildPr { //a child process spawned by shell
     pub handle: process::Command,
+    //I/O streams for redirection
     stdin: Option<Stdio>, 
     stdout: Option<Stdio>,
+
     pub prog_name: String,
     pub heredoc_content: Option<String>,
 }
@@ -80,9 +86,12 @@ then for each child in children, wait on the child.
 automatically the console should have the stdout of the last prog
  * */
 
-fn handle_cmd(cmd_buf: &str) {
-    let (cmd, mut heredocs) = extract_heredocs(cmd_buf);
-    let mut progs: Vec<&str> = cmd.split('|').collect(); 
+fn handle_cmd(cmd: &str, mut heredocs: VecDeque<String>) {
+    let progs: Vec<&str> = cmd
+        .split('|')
+        .map(|prog| prog.trim())
+        .filter(|&prog| !prog.is_empty())
+        .collect(); 
     //only one program to execute
     if progs.len() == 1 {
         if progs[0].trim().is_empty() { return; }
@@ -110,8 +119,6 @@ fn handle_cmd(cmd_buf: &str) {
     let mut cur_child: ChildPr;
     let mut child_prs: Vec<ChildPr> = Vec::with_capacity(progs.len());
     let mut pipe_setup_success = true;
-    progs.retain(|&prog| !prog.trim().is_empty()); //remove all empty commands
-                                                         //i.e. [cmd] | | [cmd] is ok
     for (i, prog) in progs.iter().enumerate() {
         if (*prog).trim().is_empty() { 
             continue; 
@@ -165,7 +172,7 @@ fn parse_program(prog: &str, heredocs: &mut VecDeque<String>) -> anyhow::Result<
     }
     let mut stdin = None;
     let mut stdout = None;
-    let mut heredoc_content = None;
+    let mut heredoc_content = String::new();
     let mut args = Vec::new();
     let mut i = 0;
     while i < tkns.len() {
@@ -197,7 +204,7 @@ fn parse_program(prog: &str, heredocs: &mut VecDeque<String>) -> anyhow::Result<
                     return Err(anyhow!("please specify a delimiter"));
                 }
                 if let Some(heredoc) = heredocs.pop_front() {
-                    heredoc_content = Some(heredoc);
+                    heredoc_content.push_str(&heredoc);
                 } else {
                     return Err(anyhow!("no heredoc body specified"));
                 }
@@ -217,7 +224,7 @@ fn parse_program(prog: &str, heredocs: &mut VecDeque<String>) -> anyhow::Result<
         stdin: stdin,
         stdout: stdout,
         prog_name: args[0].clone(),
-        heredoc_content: heredoc_content,
+        heredoc_content: if heredoc_content.is_empty() { None } else { Some(heredoc_content) }
     })
 }
 
@@ -248,155 +255,99 @@ fn expand_tilde(path: &PathBuf) -> PathBuf {
     }
 }
 
-fn validate_heredoc(cmd: &str) -> bool {
-    //if contains "<< (*)" then check if another line has the delimiter
-    //   if not then switch to heredoc continuation
-    //if has << but no delimiter, then ok
-    if let Some(idx) = cmd.find("<<") {
-        let after_arrow = cmd[idx + 2..].trim_start();
-        let delim = after_arrow.split(|c: char| c.is_whitespace() || c == '|' || c == ';')
-            .next()
-            .unwrap_or("");
-
-        if delim.is_empty() {
-            return true; // No delimiter typed yet, keep polling
-        }
-        let lines = cmd.get(idx..).unwrap_or("").split('\n');
-        for l in lines.skip(1) {
-            if l.trim_end() == delim {
-                return true 
-            }
-        }
-        return false //must switch to heredoc> continuation
-    }
-    true
-}
-
-fn extract_heredocs(cmd: &str) -> (String, VecDeque<String>) {
-    let mut heredocs = VecDeque::new();
-    let mut clean_cmd = String::new();
-    let mut lines = cmd.lines();
-    while let Some(line) = lines.next() {
-        clean_cmd.push_str(line);
-        clean_cmd.push('\n');
-        if let Some(idx) = line.find("<<") {
-            match line.get((idx+2)..) {
-                Some(text) => {
-                    let delim = text.trim_start().split(|c: char| c.is_whitespace() || c == '|' || c == ';')
-                        .next()
-                        .unwrap_or("");
-                    if !delim.is_empty() {
-                        let mut payload = String::new();
-                        while let Some(heredoc_content) = lines.next() {
-                            if heredoc_content.trim_end() == delim { break; }
-                            payload.push_str(heredoc_content);
-                            payload.push('\n');
-                        }
-                        heredocs.push_back(payload);
-                    }
-                },
-                None => continue,
-            }
+fn lex_cmd_buf(lex: &mut Lexer<Tkn>) -> Option<(usize, VecDeque<String>)> {
+    let mut cmd_length: usize = 0;
+    while let Some(res) = lex.next() {
+        match res {
+            Ok(Tkn::Newline(heredocs_)) => {
+                //first newline encountered finishes the command. (User pressed enter)
+                print!("\n");
+                cmd_length += 1; //+1 for newline
+                return Some((cmd_length, heredocs_));
+            },
+            Ok(Tkn::Quote(quoted_string)) => {
+                print!("tkn: \"{}\"; ", &quoted_string);
+                cmd_length += lex.slice().len();
+            },
+            Ok(Tkn::Whitespace) => cmd_length += lex.slice().len(),
+            Ok(_) =>  {
+                print!("tkn: {}; ", lex.slice());
+                cmd_length += lex.slice().len();
+            },
+            Err(_) => return None,
         }
     }
-    (clean_cmd, heredocs)
-}
-
-fn validate_input(cmd_: &str) -> Option<String> {
-    let cmd = cmd_.trim();
-    let mut fix_prompt = String::new();
-    match split(cmd) {
-        Ok(tkns) => {
-            if let Some(last_tkn) = tkns.last() {
-                match last_tkn.as_str() {
-                    "|" => {
-                        fix_prompt.push_str("pipe"); 
-                    },
-                    "||" => {
-                        fix_prompt.push_str("cmdor"); 
-                    },
-                    "&&" => {
-                        fix_prompt.push_str("cmdand"); 
-                    },
-                    _ => (),
-                }
-            }
-            if !validate_heredoc(cmd) {
-                fix_prompt.push_str("heredoc");
-            }
-        },
-        Err(_) => {
-            if cmd.ends_with('\\') {
-                //backslash continuation
-                fix_prompt.push_str("backslash");
-            } else {
-                //missing closing " or '
-                fix_prompt.push_str("dquote");
-            }
-        }
-    }
-    if fix_prompt.is_empty() {
-        None
-    } else {
-        fix_prompt.push_str("> ");
-        Some(fix_prompt)
-    }
+    None
 }
 
 fn main() -> rustyline::Result<()> {
-    let mut line_num = 0;
     let mut rl = DefaultEditor::new()?;
     if rl.history_mut().load(Path::new(EDITOR_HISTORY)).is_err() {
         println!("Failed to load shell command history");
     }
     let mut cmd_buf = String::new();
-    let mut cwd = env::current_dir().unwrap().file_name().unwrap().to_str().unwrap().to_string();
-    let mut prompt = format!("{}: {} % ", line_num, cwd);
+    let mut line_num = 0;
+    let mut prompt = String::new();
+    set_normal_prompt(&mut prompt, &line_num);
 
     loop {
+        line_num += 1;
         match rl.readline(&prompt) {
             Ok(input) => {
                 if input.is_empty() {continue;}
                 if input.trim() == "exit" { exit_shell(&mut rl); };
                 cmd_buf.push_str(&input);
-                if let Some(fix_prompt) = validate_input(&cmd_buf) {
-                    prompt = fix_prompt;
-                    cmd_buf.push('\n');
-                } else {
-                    cwd = env::current_dir().unwrap().file_name().unwrap().to_str().unwrap().to_string();
-                    prompt = format!("{}: {} % ", line_num, cwd);
-                    handle_cmd(&cmd_buf); //parse and execute
-                    let _ = rl.add_history_entry(&cmd_buf);
-                    cmd_buf.clear();
+                cmd_buf.push('\n'); //add back newline that readline stripped after user hit Enter
+                let lex_state = LexerState::new();
+                let mut lex = Tkn::lexer_with_extras(&cmd_buf, lex_state);
+                match lex_cmd_buf(&mut lex) {
+                    Some((cmd_length, heredocs)) => {
+                        println!("cmd_buf: {}cmd_length: {}", cmd_buf, cmd_length);
+                        handle_cmd(&cmd_buf[..cmd_length], heredocs);
+                        set_normal_prompt(&mut prompt, &line_num);
+                        let _ = rl.add_history_entry(cmd_buf.trim());
+                        cmd_buf.clear();
+                    },
+                    None => {
+                        if let Some(closer) = lex.extras.expected_closer {
+                            prompt = format!("missing {} > ", closer);
+                        } else if let Some(err) = lex.extras.syntax_err {
+                            println!("Syntax ERR: {}", err);
+                            set_normal_prompt(&mut prompt, &line_num);
+                            let _  = rl.add_history_entry(cmd_buf.trim());
+                            cmd_buf.clear();
+                        }
+                    }
                 }
             },
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
-                cwd = env::current_dir().unwrap().file_name().unwrap().to_str().unwrap().to_string();
-                prompt = format!("{}: {} % ", line_num, cwd);
+                set_normal_prompt(&mut prompt, &line_num);
                 cmd_buf.clear();
             },
             Err(ReadlineError::Eof) => {
                 println!("CTRL-D");
-                cwd = env::current_dir().unwrap().file_name().unwrap().to_str().unwrap().to_string();
-                prompt = format!("{}: {} % ", line_num, cwd);
+                set_normal_prompt(&mut prompt, &line_num);
                 cmd_buf.clear();
             },
             Err(err) => {
-                println!("error: {:?}", err);
-                break
+                println!("ERR: {:?}", err);
+                exit_shell(&mut rl);
             },
         }
-        line_num += 1;
     }
-    Ok(())
+}
+
+fn set_normal_prompt(prompt: &mut String, line_num: &usize) {
+    let cwd = env::current_dir().unwrap().file_name().unwrap().to_str().unwrap().to_string();
+    *prompt = format!("{}: {} % ", line_num, cwd);
 }
 
 fn exit_shell(rl: &mut DefaultEditor) {
-    for i in 0..rl.history().len() {
-        let entry = rl.history().get(i, SearchDirection::Forward).unwrap().unwrap().entry;
-        println!("{}: {}", i, entry);
-    }
+    //for i in 0..rl.history().len() {
+    //    let entry = rl.history().get(i, SearchDirection::Forward).unwrap().unwrap().entry;
+    //    println!("{}: {}", i, entry);
+    //}
     if rl.history_mut().save(Path::new(EDITOR_HISTORY)).is_err() {
         println!("Failed to save history");
     }
